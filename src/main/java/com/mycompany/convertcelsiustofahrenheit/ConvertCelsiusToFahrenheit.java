10721d3bef7447745e9905bbc2bb7db694366c57
smrki1 <mrkflux@gmail.com>
2022-08-28 15:59:46 +1200

    test
    test

---
+++ src/main/java/com/mycompany/convertcelsiustofahrenheit/ConvertCelsiusToFahrenheit.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.convertcelsiustofahrenheit;
+import java.util.Scanner;
+/**
+ *
+ * @author smrki
+ */
+public class ConvertCelsiusToFahrenheit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello how are you?");
+        System.out.println("Today we will convert your C to F so you can see how hot it is today!");
+        System.out.println("Please tell me the temperature in Celsius");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        //final double conversionRate = 
+        // (c*1.8) + 32
+        double tempCelsius = scanner.nextDouble();
+        
+        try {
+            double x = tempCelsius;
+        }
+        catch(Exception Type1) {
+            System.out.println("Please enter a valid number");
+        }
+        
+        double tempFahrenheit = (tempCelsius * 1.8) + 32;
+        System.out.println("Todays temerature in F is: " + tempFahrenheit );
+    }
+}
